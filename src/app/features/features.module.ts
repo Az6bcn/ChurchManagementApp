341f86da8d9fcf79140719ae00acfc5c80de0e38
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2021-11-07 23:29:47 +0000

    Members add form created

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,10 +1,38 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MembersComponent } from './members/members.component';
 import { MembersAddComponent } from './members/members-add/members-add.component';
 import { FeaturesRoutingModule } from './feature-routing.module';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { NbMenuModule, NbCardModule, NbUserModule, NbButtonModule, NbIconModule, NbTabsetModule, NbSelectModule, NbListModule, NbProgressBarModule } from '@nebular/theme';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { ChartModule } from 'angular2-chartjs';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { ThemeModule } from '../@theme/theme.module';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
-
+const materialModules = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatNativeDateModule,
+  MatDatepickerModule,
+  MatCheckboxModule,
+  MatSlideToggleModule,
+  MatRadioModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+];
 
 @NgModule({
   declarations: [
@@ -13,7 +41,24 @@ import { FeaturesRoutingModule } from './feature-routing.module';
   ],
   imports: [
     CommonModule,
-    FeaturesRoutingModule
+    ReactiveFormsModule,
+    FeaturesRoutingModule,
+    NbMenuModule,
+    ThemeModule,
+    ThemeModule,
+    NbCardModule,
+    NbUserModule,
+    NbButtonModule,
+    NbIconModule,
+    NbTabsetModule,
+    NbSelectModule,
+    NbListModule,
+    ChartModule,
+    NbProgressBarModule,
+    NgxEchartsModule,
+    NgxChartsModule,
+    LeafletModule,
+    ...materialModules
   ]
 })
 export class FeaturesModule { }
--- src/app/features/members/members-add/members-add.component.ts
+++ src/app/features/members/members-add/members-add.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembersAddComponent implements OnInit {
 
-  constructor() { }
+  memberFG: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.memberFG = this.buildForm(this.formBuilder);
+
+    this.memberFG.valueChanges.subscribe(x => console.log(x));
   }
 
+  private buildForm(fb: FormBuilder): FormGroup {
+    return fb.group({
+      'name': ['', [Validators.required]],
+      'surname': ['', [Validators.required]],
+      'dateAndMonthOfBirth': ['', [Validators.required]],
+      'gender': ['', [Validators.required]],
+      'phoneNumber': ['', [Validators.required]],
+      'isWorker': ['', [Validators.required]],
+      'tenantId': ['', [Validators.required]],
+    })
+  }
+
+
+  get name() { return this.memberFG.get('name') };
+  get surname() { return this.memberFG.get('surname') };
+  get dateMonthOfBirth() { return this.memberFG.get('dateMonthOfBirth') };
+  get gender() { return this.memberFG.get('gender') };
+  get phoneNumber() { return this.memberFG.get('phoneNumber') };
+  get isWorker() { return this.memberFG.get('isWorker') };
 }
